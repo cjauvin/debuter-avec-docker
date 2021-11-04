7048e0b77630e2a94dc98f6165d0cd88c49dc54c
Christian Jauvin <cjauvin@gmail.com>
2021-11-04 12:36:24 -0400

    Update

--- util/say_hello.py
+++ util/say_hello.py
@@ -1,16 +1,5 @@
-# import sys
-
-# name = sys.argv[1] if len(sys.argv) > 1 else 'Stranger'
-
-# print(f'Hello {name}!')
-
 import sys
-import datetime as dt
-import cowsay
 
-name = sys.argv[1] if len(sys.argv) > 1 else 'stranger'
+name = sys.argv[1] if len(sys.argv) > 1 else 'Stranger'
 
-wd = dt.datetime.today().strftime('%A')
-
-#cowsay.cow(f'Hello {name}!')
-cowsay.cow(f'Hello {name}, today is {wd}!')
+print(f'Hello {name}!')
